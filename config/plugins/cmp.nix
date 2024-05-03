5437d1c9de0310df37f34d12d9a83ef6b0e7da55
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-03 13:44:54 -0400

    dang

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,32 +1,38 @@
 {
   plugins = {
-    cmp = {
-      enable = true;
-      menu = {
-        nvim_lsp = "[LSP]";
-        nvim_lua = "[api]";
-        path = "[path]";
-        luasnip = "[snip]";
-        buffer = "[buffer]";
-        neorg = "[neorg]";
-        cmp_tabnine = "[TabNine]";
-      };
-    };
+  cmp = {
+enable = true;
+settings = {
+snippets.expand = "luasnip";
+mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<Tab>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_next_item()";
+          };
+          "<S-Tab>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_prev_item()";
+          };
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
 
-    cmp-nvim-lsp = {
-      enable = true;
-    };
-    cmp-buffer = {
-      enable = true;
-    };
-    cmp-path = {
-      enable = true;
-    };
-    cmp-cmdline = {
-      enable = true;
-    };
-    cmp_luasnip = {
-      enable = true;
-    };
+        sources = [
+          {name = "path";}
+          {name = "nvim_lsp";}
+          {name = "cmp_tabnine";}
+          {name = "luasnip";}
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested.
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          {name = "neorg";}
+        ];
+};
+  };
   };
 }
--- config/plugins/lspkind.nix
+++ config/plugins/lspkind.nix
@@ -1,5 +1,20 @@
 {
   plugins.lspkind = {
     enable = true;
+
+    cmp = {
+      enable = true;
+      menu = {
+        nvim_lsp = "[LSP]";
+        nvim_lua = "[api]";
+        path = "[path]";
+        luasnip = "[snip]";
+        buffer = "[buffer]";
+        neorg = "[neorg]";
+        cmp_tabnine = "[TabNine]";
+      };
+    };
+
+
   };
 }
