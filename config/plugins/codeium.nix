6745191684111057f3a8696a27d3d9a7bbe655b3
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-03 15:13:13 -0400

    try this again

--- config/plugins/codeium.nix
+++ config/plugins/codeium.nix
@@ -1,5 +1,7 @@
 {
   plugins.codeium-nvim = {
     enable = true;
+    configPath.__raw = "vim.fn.stdpath('cache') .. '/codeium/config.json'";
+    binPath.__raw = "vim.fn.stdpath('cache') .. '/codeium/bin'";
   };
 }
