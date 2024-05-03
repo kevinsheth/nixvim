55d843a29023ca47cda0e8809fda397f9dae463d
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-03 14:57:58 -0400

    maybe this

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -5,16 +5,16 @@
       settings = {
         snippets.expand = "luasnip";
         mapping = {
-            __raw = ''
-              cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-              })
-            '';
-          };
+          __raw = ''
+            cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            })
+          '';
+        };
         sources = [
           {name = "path";}
           {name = "nvim_lsp";}
@@ -26,6 +26,7 @@
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
           }
           {name = "neorg";}
+          {name = "codeium";}
         ];
       };
     };
