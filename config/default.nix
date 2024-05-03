16f98c34b6af5ae3019b13d6f55f6e9c441bc295
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-03 15:51:02 -0400

    there we go

--- config/default.nix
+++ config/default.nix
@@ -11,16 +11,15 @@
       mapleader = " ";
       maplocalleader = " ";
     };
-  };
-
-  options = {
-    cmdheight = 2;
-    guifont = "monospace:h17";
-    shiftwidth = 2;
-    tabstop = 2;
-    relativenumber = true;
-    wrap = true;
-    number = true;
-    showcmd = true;
+    opts = {
+      cmdheight = 2;
+      guifont = "monospace:h17";
+      shiftwidth = 2;
+      tabstop = 2;
+      relativenumber = true;
+      wrap = true;
+      number = true;
+      showcmd = true;
+    };
   };
 }
--- config/plugins/alpha.nix
+++ config/plugins/alpha.nix
@@ -1,5 +1,70 @@
 {
   plugins.alpha = {
-    enable = false;
+    enable = true;
+
+    iconsEnabled = true;
+    layout = [
+      {
+        type = "padding";
+        val = 2;
+      }
+      {
+        type = "text";
+        val = [
+          "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+          "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+          "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+          "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+          "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+          "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+        ];
+        opts = {
+          position = "center";
+          hl = "Type";
+        };
+      }
+      {
+        type = "padding";
+        val = 2;
+      }
+      {
+        type = "group";
+        val = [
+          {
+            type = "button";
+            val = "  New file";
+            on_press.__raw = "function() vim.cmd[[ene]] end";
+            opts.shortcut = "n";
+          }
+          {
+            type = "button";
+            val = " Quit Neovim";
+            on_press.__raw = "function() vim.cmd[[qa]] end";
+            opts.shortcut = "q";
+          }
+        ];
+      }
+      {
+        type = "padding";
+        val = 2;
+      }
+      {
+        type = "text";
+        val = "Inspiring quote here.";
+        opts = {
+          position = "center";
+          hl = "Keyword";
+        };
+      }
+    ];
+    opts = {
+      margin = 0;
+      noautocmd = true;
+
+      keymap = {
+        press = "<CR>";
+        press_queue = "<M-CR>";
+      };
+    };
   };
 }
