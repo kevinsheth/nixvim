eb3619ab15ea3f1fffe24889e4df25656c032c04
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-03 13:19:14 -0400

    try this

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,14 +1,50 @@
 {
   plugins = {
-    cmp.enable = true;
 
-    cmp.settings = {
-      completion.autocomplete = true;
-    };
-
-    cmp-nvim-lsp.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp_luasnip.enable = true;
+  cmp = {
+        enable = true;
+	autoEnableSources = false;
+        settings = {
+          completion = {
+            autocomplete = [ "require('cmp.types').cmp.TriggerEvent.TextChanged" ];
+          };
+          mapping = {
+            __raw = ''
+              cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              })
+            '';
+          };
+          sources.__raw = ''
+              cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'cmdline' },
+                { name = 'path' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+              })
+          '';
+        };
+      };
+      cmp-nvim-lsp = {
+        enable = true;
+      };
+      cmp-buffer = {
+        enable = true;
+      };
+      cmp-path = {
+        enable = true;
+      };
+      cmp-cmdline = {
+        enable = true;
+      };
+      cmp_luasnip = {
+        enable = true;
+      };
+      
   };
 }
