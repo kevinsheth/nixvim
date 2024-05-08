f125f0bf991d824f404bbc3a34c3c793d6bd0f13
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-08 10:31:37 -0400

    rust

--- config/plugins/dap.nix
+++ config/plugins/dap.nix
@@ -1,5 +1,8 @@
 {
   plugins.dap = {
     enable = true;
+    extensions.dap-virtual-text.enable = true;
+    extensions.dap-python.enable = true;
+    extensions.dap-ui.enable = true;
   };
 }
---
+++ config/plugins/rust.nix
@@ -0,0 +1,12 @@
+{
+  plugins.bacon = {
+    enable = true;
+  };
+
+  plugins.rustaceanvim = {
+    enable = true;
+  };
+  plugins.crates-nvim = {
+    enable = true;
+  };
+}
