00acecd4367c1a022a3b5f742bccd239542c8c66
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-09 14:51:24 -0400

    oops

--- config/plugins/python.nix
+++ config/plugins/python.nix
@@ -3,6 +3,6 @@
     lsp.servers.pyright.enable = true;
     lsp.servers.ruff-lsp.enable = true;
     none-ls.sources.formatting.black.enable = true;
-    none-ls.diagnostics.mypy.enable = true;
+    none-ls.sources.diagnostics.mypy.enable = true;
   };
 }
